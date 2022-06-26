c838dfdcba0d297c123dba7cca1cc3589286b16c
amitchauras <80785312+amitchauras@users.noreply.github.com>
2022-06-27 00:17:34 +0530

    Update Employee.Component.jsx

--- src/EmployeeDropdown/Employee.Component.jsx
+++ src/EmployeeDropdown/Employee.Component.jsx
@@ -6,24 +6,24 @@ import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Checkbox from './Checkbox';
 
 export const EmployeeDropdown = () => {
     const searchInput = useRef();
     const [personName, setPersonName] = useState('');
     const [click, setClick] = useState(true);
+    const [employeeDetail, setEmployeeDetail] = useState(EmployeeData)
 
     const handleChange = (...args) => {
-        debugger
-        console.log("ARGS:", args);
         setPersonName(args[1].name)
     };
 
     const handleFilter = () => {
         return (searchValue) => {
             if (searchValue.length === 0) {
-                return EmployeeData;
+                return employeeDetail;
             }
-            const updatedItems = EmployeeData.map((list) => {
+            const updatedItems = employeeDetail.map((list) => {
                 const newItems = list.items.filter((item) => {
                     return item.name.toLowerCase().includes(searchValue.toLowerCase());
                 });
@@ -46,15 +46,75 @@ export const EmployeeDropdown = () => {
                         <InputAdornment position="end">{iconEnd}</InputAdornment>
                     ) : null
                 }}
-                style={{width: '300px'}}
+                style={{ width: '300px' }}
             />
         );
     };
 
     const employeeDataLength = () => {
-        const Employee = EmployeeData.map((item) => item.items.length)
+        const Employee = employeeDetail.map((item) => item.items.length)
         const EmployeeLength = Employee.reduce((partialSum, a) => partialSum + a, 0);
         return EmployeeLength
+    }
+    const handleAllChecked = (event) => {
+        const currentEmployeeData = employeeDetail
+        const updatedItems = currentEmployeeData.map((list) => {
+            if (list.name == event.target.value){
+                list.disabled = true;
+            } else list.disabled = false;
+            return list
+        });
+        const filteredData = updatedItems.filter((item) => item.disabled === true)
+        const updatedFilteredData = filteredData.map((filterData) => {
+            return filterData.items
+        })
+        updatedFilteredData.map((item) => {
+            const basedItem = item.map((based) => {
+                    based.disabled = event.target.checked;
+        })
+            return basedItem
+        });
+        setEmployeeDetail(currentEmployeeData);
+    };
+    const handleCheckChieldElement = (event) => {
+        const currentEmployeeData = employeeDetail
+        const updatedItems = currentEmployeeData.map((list) => {
+            return list
+        });
+        const updatedFilteredData = updatedItems.map((filterData) => {
+            return filterData.items
+        })
+        updatedFilteredData.map((item) => {
+            const filteredData = item.filter((item) => item.name === event.target.value)
+            const basedItem = filteredData.map((based) => {
+                if (based.name == event.target.value){
+                    based.disabled = event.target.checked;
+                }
+        })
+            return basedItem
+        });
+        setEmployeeDetail(currentEmployeeData);
+    };
+    const headerCheckbox = (optionValue) => {
+        
+        return (
+            <div>
+                <input
+                    type="checkbox"
+                    onClick={(e) => handleAllChecked(e)}
+                    value={optionValue}
+                />{" "}
+                <label>{optionValue}</label>
+            </div>
+        );
+    }
+    const Option = (optionValue) => {
+        return (
+            <Checkbox
+                handleCheckChieldElement={handleCheckChieldElement}
+                {...optionValue}
+            />
+        );
     }
     return (
         <>
@@ -77,9 +137,9 @@ export const EmployeeDropdown = () => {
                         />
                         <SelectSearch
                             ref={searchInput}
-                            options={EmployeeData}
                             filterOptions={handleFilter}
-                            value=""
+                            renderOption={(optionValue) => Option(optionValue)}
+                            renderGroupHeader={(name) => headerCheckbox(name)}
                             name="Employee"
                             placeholder="Select Employee"
                             search
